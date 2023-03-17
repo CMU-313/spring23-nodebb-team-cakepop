4c8991be283a8e35a1ffc29e6e5c08fc2aba4ee7
Maddie Thai-Tang <mthaitan@andrew.cmu.edu>
2023-03-16 21:45:28 -0400

    iroh test updates, currently getting constructor type error

--- iroh/index.js
+++
@@ -1,9 +0,0 @@
-/* eslint-disable */
-'use strict';
-
-const Iroh = require('iroh');
-const utils = require('../src/utils.js');
-
-let stage = new Iroh.stage(`utils.isEmailValid("emailemail@gmail.com");`);
-
-eval(stage.script);
