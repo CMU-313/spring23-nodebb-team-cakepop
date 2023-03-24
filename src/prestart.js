ee94f573cc7386431195d7e4ca0251d15f1679d6
Alex Isparyan <aisparyan@gmail.com>
2023-03-23 18:55:49 -0700

    added new constructor in prestart.js:90

--- src/prestart.js
+++ src/prestart.js
@@ -87,7 +87,7 @@ function loadConfig(configFile) {
 
     if (nconf.get('url')) {
         nconf.set('url', nconf.get('url').replace(/\/$/, ''));
-        nconf.set('url_parsed', url.URL(nconf.get('url')));
+        nconf.set('url_parsed', new url.URL(nconf.get('url')));
         // Parse out the relative_url and other goodies from the configured URL
         const urlObject = new url.URL(nconf.get('url'));
         const relativePath = urlObject.pathname !== '/' ? urlObject.pathname.replace(/\/+$/, '') : '';
